75078cbf997173a043d88be4aa29349799385857
Ghent Photography <hello@ghentphotography.be>
2023-07-08 19:52:14 +0200

    Initial Commit

---
+++ GP AppTests/GP_AppTests.swift
@@ -0,0 +1,36 @@
+//
+//  GP_AppTests.swift
+//  GP AppTests
+//
+//  Created by Dave Van Cauwenberghe on 08/07/2023.
+//
+
+import XCTest
+@testable import GP_App
+
+final class GP_AppTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        // Any test you write for XCTest can be annotated as throws and async.
+        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
+        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
