af1311f2cf7d741060dffb8c1053c1550ccb9860
“appledave”vc <davevancauwenberghe@outlook.com>
2023-08-02 03:47:17 +0200

    - Settings VC added

---
+++ GP App/Settings/SettingsViewCell.swift
@@ -0,0 +1,50 @@
+//
+//  SettingsViewCell.swift
+//  GP App
+//
+//  Created by Dave Van Cauwenberghe on 02/08/2023.
+//
+
+import UIKit
+
+class SettingsViewCell: UITableViewCell {
+    @IBOutlet weak var iconContainerView: UIView!
+    @IBOutlet weak var iconImageView: UIImageView!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var chevronImageView: UIImageView!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+
+        // Set label properties to prevent truncation and adjust layout
+        titleLabel.numberOfLines = 0 // Allow multiple lines
+        titleLabel.lineBreakMode = .byTruncatingTail
+        titleLabel.setContentCompressionResistancePriority(.required, for: .vertical) // Adjust priority
+
+        // Set the chevron image
+        chevronImageView.image = UIImage(systemName: "chevron.right")
+
+        // Adjust the constraints to center the icon vertically
+        iconContainerView.translatesAutoresizingMaskIntoConstraints = false
+        iconContainerView.widthAnchor.constraint(equalTo: iconContainerView.heightAnchor).isActive = true
+        iconContainerView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 16).isActive = true
+        iconContainerView.centerYAnchor.constraint(equalTo: contentView.centerYAnchor).isActive = true
+        iconContainerView.heightAnchor.constraint(equalToConstant: 24).isActive = true
+
+        iconImageView.translatesAutoresizingMaskIntoConstraints = false
+        iconImageView.centerXAnchor.constraint(equalTo: iconContainerView.centerXAnchor).isActive = true
+        iconImageView.centerYAnchor.constraint(equalTo: iconContainerView.centerYAnchor).isActive = true
+        iconImageView.widthAnchor.constraint(equalToConstant: 24).isActive = true
+        iconImageView.heightAnchor.constraint(equalTo: iconImageView.widthAnchor).isActive = true
+
+        titleLabel.translatesAutoresizingMaskIntoConstraints = false
+        titleLabel.leadingAnchor.constraint(equalTo: iconContainerView.trailingAnchor, constant: 16).isActive = true
+        titleLabel.centerYAnchor.constraint(equalTo: contentView.centerYAnchor).isActive = true
+
+        chevronImageView.translatesAutoresizingMaskIntoConstraints = false
+        chevronImageView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -16).isActive = true
+        chevronImageView.centerYAnchor.constraint(equalTo: contentView.centerYAnchor).isActive = true
+        chevronImageView.widthAnchor.constraint(equalToConstant: 20).isActive = true
+        chevronImageView.heightAnchor.constraint(equalTo: chevronImageView.widthAnchor).isActive = true
+    }
+}
---
+++ GP App/Settings/SettingsViewController.swift
@@ -0,0 +1,133 @@
+//
+//  SettingsViewController.swift
+//  GP App
+//
+//  Created by Dave Van Cauwenberghe on 02/08/2023.
+//
+
+import UIKit
+
+class SettingsViewController: UITableViewController {
+
+    let sections = ["General", "Appearance", "About GP app"]
+    let generalOptions = ["Push Notifications"]
+    let appearanceOptions = ["Dark Mode", "App Icon", "App Theme"]
+    let aboutOptions = ["Release notes", "Thank you"]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupHeaderView()
+        tableView.tableFooterView = UIView() // To hide empty rows at the bottom
+    }
+    
+    private func setupHeaderView() {
+        let headerView = UIView(frame: CGRect(x: 0, y: 0, width: view.bounds.width, height: 160))
+        
+        // Add the AppIconImageView
+        let appIconImageView = UIImageView(image: UIImage(named: "AppIcon"))
+        appIconImageView.translatesAutoresizingMaskIntoConstraints = false
+        appIconImageView.contentMode = .scaleAspectFill
+        appIconImageView.layer.cornerRadius = 24 // You can adjust this value to make it bigger
+        appIconImageView.clipsToBounds = true // Clip the image to avoid white space
+        headerView.addSubview(appIconImageView)
+        
+        // Add the "Dave Van Cauwenberghe - v1.5.1" label
+        let versionLabel = UILabel()
+        versionLabel.translatesAutoresizingMaskIntoConstraints = false
+        versionLabel.text = "© Dave Van Cauwenberghe - v1.5.1"
+        versionLabel.textAlignment = .center
+        versionLabel.font = UIFont.systemFont(ofSize: 14, weight: .regular)
+        versionLabel.textColor = .darkGray
+        headerView.addSubview(versionLabel)
+        
+        NSLayoutConstraint.activate([
+            // AppIconImageView constraints
+            appIconImageView.centerXAnchor.constraint(equalTo: headerView.centerXAnchor),
+            appIconImageView.centerYAnchor.constraint(equalTo: headerView.centerYAnchor),
+            appIconImageView.widthAnchor.constraint(equalToConstant: 100), // Adjust this size as needed
+            appIconImageView.heightAnchor.constraint(equalTo: appIconImageView.widthAnchor), // Aspect ratio is preserved
+            
+            // Version label constraints
+            versionLabel.centerXAnchor.constraint(equalTo: headerView.centerXAnchor),
+            versionLabel.topAnchor.constraint(equalTo: appIconImageView.bottomAnchor, constant: 8), // Adjust the spacing as needed
+        ])
+        
+        tableView.tableHeaderView = headerView
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return sections.count
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        switch section {
+        case 0:
+            return generalOptions.count
+        case 1:
+            return appearanceOptions.count
+        case 2:
+            return aboutOptions.count
+        default:
+            return 0
+        }
+    }
+    
+    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return sections[section]
+    }
+    
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 60
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "SettingsCell", for: indexPath) as! SettingsViewCell
+        
+        switch indexPath.section {
+        case 0:
+            cell.iconImageView.image = UIImage(systemName: "lock.shield.fill") // Example icon for "Push Notifications"
+            cell.titleLabel.text = generalOptions[indexPath.row]
+        case 1:
+            switch indexPath.row {
+            case 0:
+                if let image = UIImage(systemName: "moon.fill") {
+                    cell.iconImageView.image = image // Example icon for "Dark Mode"
+                }
+                cell.titleLabel.text = appearanceOptions[indexPath.row]
+            case 1:
+                if let image = UIImage(systemName: "app.badge") {
+                    cell.iconImageView.image = image // Example icon for "App Icon"
+                }
+                cell.titleLabel.text = appearanceOptions[indexPath.row]
+            case 2:
+                if let image = UIImage(systemName: "paintpalette.fill") {
+                    cell.iconImageView.image = image // Example icon for "App Theme"
+                }
+                cell.titleLabel.text = appearanceOptions[indexPath.row]
+            default:
+                break
+            }
+        case 2:
+            switch indexPath.row {
+            case 0:
+                if let image = UIImage(systemName: "doc.text.fill") {
+                    cell.iconImageView.image = image // Example icon for "Release notes"
+                }
+                cell.titleLabel.text = aboutOptions[indexPath.row]
+            case 1:
+                if let image = UIImage(systemName: "heart.fill") {
+                    cell.iconImageView.image = image // Example icon for "Thank you"
+                }
+                cell.titleLabel.text = aboutOptions[indexPath.row]
+            default:
+                break
+            }
+        default:
+            break
+        }
+        
+        return cell
+    }
+}
