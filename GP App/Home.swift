b666270897a2dfa8d6b7e827acd3e359535fc2a8
“appledave”vc <davevancauwenberghe@outlook.com>
2023-08-11 03:25:16 +0200

    - UI changes

--- GP App/Home.swift
+++ GP App/Home.swift
@@ -11,8 +11,23 @@ class Home: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        // Set navigation bar background image
+        let backgroundImage = UIImage(named: "Orange")
+        navigationController?.navigationBar.setBackgroundImage(backgroundImage, for: .default)
+        
+        // Configure large title
+        navigationController?.navigationBar.prefersLargeTitles = true
+        navigationItem.largeTitleDisplayMode = .always
+        
+        // Set large title attributes
+        navigationController?.navigationBar.largeTitleTextAttributes = [
+            .foregroundColor: UIColor.white
+        ]
+        
+        // Enable scroll edge appearance
+        navigationController?.navigationBar.scrollEdgeAppearance = navigationController?.navigationBar.standardAppearance
+        
+        // Do any additional setup for your Home view controller...
     }
-
-
 }
