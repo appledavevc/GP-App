31b3728db40e4e5cc3e08a08c86b25acb61d8d64
“appledave”vc <davevancauwenberghe@outlook.com>
2023-08-12 03:46:05 +0200

    - New "Home"

--- GP App/Home.swift
+++ GP App/Home.swift
@@ -8,26 +8,87 @@
 import UIKit
 
 class Home: UIViewController {
-
+    
+    let scrollView: UIScrollView = {
+        let scrollView = UIScrollView()
+        scrollView.translatesAutoresizingMaskIntoConstraints = false
+        return scrollView
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // Set navigation bar background image
-        let backgroundImage = UIImage(named: "Orange")
-        navigationController?.navigationBar.setBackgroundImage(backgroundImage, for: .default)
+        view.addSubview(scrollView)
         
-        // Configure large title
-        navigationController?.navigationBar.prefersLargeTitles = true
-        navigationItem.largeTitleDisplayMode = .always
+        NSLayoutConstraint.activate([
+            scrollView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            scrollView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            scrollView.topAnchor.constraint(equalTo: view.topAnchor),
+            scrollView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+        ])
         
-        // Set large title attributes
-        navigationController?.navigationBar.largeTitleTextAttributes = [
-            .foregroundColor: UIColor.white
-        ]
+        let buttonSize = CGSize(width: view.bounds.width - 32, height: view.bounds.width - 32)
+        let spacing: CGFloat = 16
         
-        // Enable scroll edge appearance
-        navigationController?.navigationBar.scrollEdgeAppearance = navigationController?.navigationBar.standardAppearance
+        var previousButton: UIButton?
         
-        // Do any additional setup for your Home view controller...
+        for imageName in ["Thumb1", "Thumb2", "Thumb3"] {
+            let button = createThumbButton(withImage: imageName)
+            scrollView.addSubview(button)
+            
+            NSLayoutConstraint.activate([
+                button.widthAnchor.constraint(equalToConstant: buttonSize.width),
+                button.heightAnchor.constraint(equalToConstant: buttonSize.height),
+                button.centerXAnchor.constraint(equalTo: scrollView.centerXAnchor),
+            ])
+            
+            if let previous = previousButton {
+                NSLayoutConstraint.activate([
+                    button.topAnchor.constraint(equalTo: previous.bottomAnchor, constant: spacing)
+                ])
+            } else {
+                NSLayoutConstraint.activate([
+                    button.topAnchor.constraint(equalTo: scrollView.topAnchor, constant: spacing)
+                ])
+            }
+            
+            previousButton = button
+        }
+        
+        // Set the content size of the scroll view
+        scrollView.contentSize = CGSize(width: view.bounds.width, height: buttonSize.height * CGFloat(3) + spacing * CGFloat(2))
+    }
+    
+    func createThumbButton(withImage imageName: String) -> UIButton {
+        let button = UIButton()
+        button.setImage(UIImage(named: imageName), for: .normal)
+        button.imageView?.contentMode = .scaleAspectFill
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.layer.cornerRadius = 15
+        button.layer.borderWidth = 2
+        button.layer.borderColor = UIColor.systemBlue.cgColor
+        button.clipsToBounds = true
+        
+        // Add chevron image view to the button
+        let chevronImageView = UIImageView(image: UIImage(systemName: "chevron.right"))
+        chevronImageView.tintColor = .white
+        chevronImageView.contentMode = .center
+        chevronImageView.translatesAutoresizingMaskIntoConstraints = false
+        button.addSubview(chevronImageView)
+        
+        NSLayoutConstraint.activate([
+            chevronImageView.centerYAnchor.constraint(equalTo: button.centerYAnchor),
+            chevronImageView.trailingAnchor.constraint(equalTo: button.trailingAnchor, constant: -12),
+            chevronImageView.widthAnchor.constraint(equalToConstant: 20),
+            chevronImageView.heightAnchor.constraint(equalToConstant: 20)
+        ])
+        
+        button.addTarget(self, action: #selector(thumbButtonTapped), for: .touchUpInside)
+        return button
+    }
+    
+    @objc func thumbButtonTapped(sender: UIButton) {
+        let portfolioTabBarController = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "PortfolioTabBarController") as! PortfolioTabBarController
+        navigationController?.pushViewController(portfolioTabBarController, animated: true)
     }
 }
---
+++ GP App/Portfolio/PortfolioTabBarController.swift
@@ -0,0 +1,35 @@
+//
+//  PortfolioTabBarController.swift
+//  GP App
+//
+//  Created by Dave Van Cauwenberghe on 11/08/2023.
+//
+
+import UIKit
+
+class PortfolioTabBarController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Set the navigation title to "Portfolio" with large title style
+        navigationItem.title = "Portfolio"
+        navigationController?.navigationBar.prefersLargeTitles = true
+        
+        // Retrieve the storyboard instance
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        
+        // Instantiate view controllers from storyboard using their identifiers
+        let featuredVC = storyboard.instantiateViewController(withIdentifier: "FeaturedViewController")
+        let newVC = storyboard.instantiateViewController(withIdentifier: "NewViewController")
+        let landscapeVC = storyboard.instantiateViewController(withIdentifier: "LandscapeViewController")
+        let portraitVC = storyboard.instantiateViewController(withIdentifier: "PortraitViewController")
+        let vaultEntryVC = storyboard.instantiateViewController(withIdentifier: "TheVaultEntryViewController")
+        
+        // Set the view controllers for the tab bar
+        viewControllers = [featuredVC, newVC, landscapeVC, portraitVC, vaultEntryVC]
+        
+        // Customize tab bar appearance if needed
+        tabBar.tintColor = .systemBlue
+    }
+}
