4344fb4ff3deab847e3c688aa1ffcb9a05562488
“appledave”vc <davevancauwenberghe@outlook.com>
2023-10-08 23:53:02 +0200

    - Alternate app icons functional

--- GP App/Settings/Appearance/IconChangeViewController.swift
+++ GP App/Settings/Appearance/IconChangeViewController.swift
@@ -9,11 +9,15 @@ import UIKit
 
 class IconChangeViewController: UIViewController {
     
-    let iconNames = [nil, "AppIcon1", "AppIcon2"] // nil represents the default icon
+    // An array of icon names (using optional String to allow nil for the default icon)
+    let iconNames: [String?] = [nil, "AppIcon1", "AppIcon2"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
         setupUI()
+        
+        // Set the title in the navigation bar
+        self.title = "App Icon"
     }
     
     private func setupUI() {
@@ -34,24 +38,28 @@ class IconChangeViewController: UIViewController {
     }
     
     @objc private func iconChanged(_ sender: UISegmentedControl) {
+        // Obtain the icon name from the array using the selected segment index
         let iconName = iconNames[sender.selectedSegmentIndex]
+        // Attempt to change the app icon
         changeIcon(to: iconName)
     }
     
     private func changeIcon(to name: String?) {
         // Check if the app supports changing icons
         guard UIApplication.shared.supportsAlternateIcons else {
+            print("App does not support alternate icons.") // Log if not supported
             return
         }
         
         // Change the icon to a specific image with the given name
         UIApplication.shared.setAlternateIconName(name) { error in
             if let error = error {
+                // Log the error if the icon change fails
                 print("App icon failed to change due to: \(error.localizedDescription)")
             } else {
+                // Log the success if the icon change is successful
                 print("App icon changed successfully!")
             }
         }
     }
 }
-
